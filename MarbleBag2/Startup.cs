31c38f6910f57775bb798f8fc815f7d563e983c2
moevegas <moevegas@gmail.com>
2017-05-16 14:53:01 -0400

    Add project files.

---
+++ MarbleBag2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MarbleBag2.Startup))]
+namespace MarbleBag2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
